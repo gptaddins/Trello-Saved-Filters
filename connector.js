fb661550bf68d2d2dc2860b50ffbc4189fa7361e
gptaddins <gptaddins@gmail.com>
2025-08-10 20:26:32 +0300

    Create connector.js

---
+++ connector.js
@@ -0,0 +1,25 @@
+window.TrelloPowerUp.initialize({
+  'board-buttons': function(t, options) {
+    return [{
+      text: 'Saved Filters',
+      callback: function(t) {
+        return t.popup({
+          title: 'Saved Filters',
+          url: './popup.html',
+          height: 300
+        });
+      }
+    }];
+  },
+  'card-badges': function(t, options) {
+    return t.get('board', 'private', 'savedFilters')
+      .then(function(data) {
+        if (data && data.activeFilter && data.filteredCardIds.includes(options.card.id)) {
+          return [{ text: 'Filtered', color: 'blue' }];
+        } else {
+          return [];
+        }
+      })
+      .catch(() => []);
+  }
+});
